55329c93637cc619e6ae142331ea4603de61ba31
Miguel <magistrado.miguel2005@gmail.com>
2026-07-22 20:27:22 +0800

    simple interest rate calculator

--- gitPractice.py
+++ gitPractice.py
@@ -1,1 +1,9 @@
-print("hello world")
+def add(a, b):
+    balance = a 
+    rate = b
+    total = balance + (balance * rate)
+    return total
+
+a = int(input("Enter the balance: "))
+b = float(input("Enter the interest rate (as a decimal): "))
+print(add(a, b))

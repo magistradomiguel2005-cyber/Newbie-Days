d56a34743856fbb64b650a138f8d3792979f11f4
Miguel <magistrado.miguel2005@gmail.com>
2026-07-25 22:57:05 +0800

    Simple High School Level checker

---
+++ HsGrades.py
@@ -0,0 +1,13 @@
+grade = int(input("Enter what grade are you in highschool: "))
+
+if grade == 9:
+  print("Freshman")
+elif grade == 10:
+  print("Sophomore")
+elif grade == 11:
+  print("Junior")
+elif grade == 12:
+  print("Senior")
+else:
+  print("TBD")
+
